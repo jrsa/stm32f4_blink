9ba2f67e59d8b5abf41ee581ce442e308f607911
James Anderson <jrsa@jrsa.co>
2017-07-24 04:55:48 -0700

    initialze project from the libopencm3 examples repsitory. updated stm32/f4/nucleo-f411re/blink example for the f401re board, by manually editing the generated linker script. make flash does not work for some reason

---
+++ test.c
@@ -0,0 +1,25 @@
+
+#include <libopencm3/stm32/rcc.h>
+#include <libopencm3/stm32/gpio.h>
+
+static void gpio_setup(void)
+{
+	rcc_periph_clock_enable(RCC_GPIOA);
+	gpio_mode_setup(GPIOA, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE, GPIO5);
+}
+
+int main(void)
+{
+	int i;
+
+	gpio_setup();
+
+	while (1) {
+		gpio_toggle(GPIOA, GPIO5);	/* LED on/off */
+		for (i = 0; i < 1000000; i++) {	/* Wait a bit. */
+			__asm__("nop");
+		}
+	}
+
+	return 0;
+}
